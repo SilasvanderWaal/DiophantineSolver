9626fc5f18d2c77af64949b522ab138f72384e40
Silas van der Waal <silasvanderwaal@gmail.com>
2024-09-08 17:37:14 +0200

    Added a template system: functions for both creating and switching templates. Also improved naming of variables and functions. Added comments to the less obvious functions

--- src/main/java/Engine/Program.java
+++ src/main/java/Engine/Program.java
@@ -1,68 +1,143 @@
 package Engine;
 
 import GUI.MainFrame;
-
 import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Program extends JFrame {
-    private int[] weld_prices = {255, 278, 348,395, 56, 61, 65, 69, 244, 278, 285, 299, 62, 65, 70, 78};
-    private int[] amount_of_welds;
-    private DefaultListModel<String> buff;
-    private int total_weld_prices = 2908;
+    private int[] variables;
+    private int[] coefficients;
+    private DefaultListModel<String> calculation_result_buff;
+    private int variables_sum;
+    int total_amount_of_variables;
+    int average_amount_of_variables;
+    int range_min;
+    int range_max;
 
-    public DefaultListModel<String> solve_diophantine(int total_amount){
-        int total_amount_of_welds = weld_prices.length;
-        amount_of_welds = new int[total_amount_of_welds];
-        System.out.println(total_weld_prices);
-        int average_amount_of_welds = total_amount / total_weld_prices;
-        int range_min = average_amount_of_welds;
-        int range_max = average_amount_of_welds + 4;
-        buff = new DefaultListModel<>();
+    public Program() {
+        ArrayList<String> list_of_strings;
 
-        if(solveDiophantine(weld_prices, total_amount_of_welds, total_amount, amount_of_welds, 0, total_amount, range_min, range_max )){
-            return buff;
+        try {
+            // Loop until a non-empty file is selected
+            do {
+                FileDialog dialog = new FileDialog((Frame) null, "Select file to open");
+                dialog.setMode(FileDialog.LOAD);
+                dialog.setVisible(true);
+                File file = new File(dialog.getDirectory(), dialog.getFile());
+                dialog.dispose();
+
+                BufferedReader bf = new BufferedReader(new FileReader(file));
+                list_of_strings = new ArrayList<>();
+                String line = bf.readLine();
+
+                while (line != null) {
+                    list_of_strings.add(line);
+                    line = bf.readLine();
+                }
+                bf.close();
+
+                if (list_of_strings.isEmpty())
+                    JOptionPane.showMessageDialog(getRootPane(), "Template is corrupt, please select / create a new template");
+            } while (list_of_strings.isEmpty());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        return buff;
+        // Initialize variables from file
+        total_amount_of_variables = list_of_strings.size();
+        variables = new int[total_amount_of_variables];
+        variables_sum = 0;
+        for (int i = 0; i < total_amount_of_variables; i++) {
+            variables[i] = Integer.parseInt(list_of_strings.get(i));
+            variables_sum += variables[i];
+        }
     }
 
-    // Rekursiv funktion för att hitta heltalslösningar till en diofantisk ekvation
-    private boolean solveDiophantine(int[] a, int n, int b, int[] x, int index, int remaining, int range_min, int range_max) {
+    public DefaultListModel<String> diophantine_bruteforce_setup(int total_amount) {
+        coefficients = new int[total_amount_of_variables];
+        average_amount_of_variables = total_amount / variables_sum;
+        range_min = average_amount_of_variables;
+        range_max = average_amount_of_variables + 2;
+        calculation_result_buff = new DefaultListModel<>();
+
+        if (total_amount <= variables_sum) {
+            JOptionPane.showMessageDialog(getRootPane(), "The amount in EUROS must be more than the sum of all the variables");
+            return calculation_result_buff;
+        }
+
+        // Try small-range brute-force first
+        if (solve_diophantine(variables, total_amount_of_variables, coefficients, 0, total_amount, range_min, range_max)) {
+            return calculation_result_buff;
+        }
+
+        // Try larger-range brute-force if small-range fails
+        if (solve_diophantine(variables, total_amount_of_variables, coefficients, 0, total_amount)) {
+            return calculation_result_buff;
+        }
+
+        JOptionPane.showMessageDialog(getRootPane(), "Unable to find a solution to the equation");
+        return calculation_result_buff;
+    }
+
+    private boolean solve_diophantine(int[] a, int n, int[] x, int index, int remaining, int range_min, int range_max) {
+        // If all variables are processed and remaining amount is 0, solution found
         if (index == n) {
             if (remaining == 0) {
                 for (int i = 0; i < n; i++) {
-                        buff.addElement(x[i] + "st av " + a[i] + " EURO");
+                    calculation_result_buff.addElement(x[i] + " pcs of " + a[i] + " EURO");
                 }
                 return true;
             }
             return false;
         }
 
+        // Try different values in the given range
         for (int i = range_min; i <= range_max; i++) {
             x[index] = i;
-            if (solveDiophantine(a, n, b, x, index + 1, remaining - a[index] * i, range_min, range_max)) {
-                return true ;
+            if (solve_diophantine(a, n, x, index + 1, remaining - a[index] * i, range_min, range_max)) {
+                return true;
             }
         }
-
         return false;
     }
 
-    public void change_equation(int[] weld_prices){
-        int sum = 0;
-        this.weld_prices = weld_prices;
-
-
-        for (int i = 0; i < weld_prices.length; i++) {
-            sum += weld_prices[i];
+    private boolean solve_diophantine(int[] a, int n, int[] x, int index, int remaining) {
+        // Try larger coefficient range if smaller range fails
+        if (index == n) {
+            if (remaining == 0) {
+                for (int i = 0; i < n; i++) {
+                    calculation_result_buff.addElement(x[i] + " pcs of " + a[i] + " EURO");
+                }
+                return true;
+            }
+            return false;
         }
 
-        total_weld_prices = sum;
+        for (int i = average_amount_of_variables - 10; i <= average_amount_of_variables + 10; i++) {
+            x[index] = i;
+            if (solve_diophantine(a, n, x, index + 1, remaining - a[index] * i)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public int[] get_amount_of_welds(){
-        return amount_of_welds;
+    public void change_equation(int[] variables) {
+        variables_sum = 0;
+        this.variables = variables;
+
+        for (int i = 0; i < variables.length; i++) {
+            variables_sum += variables[i];
+        }
+
+        total_amount_of_variables = variables.length;
+    }
+
+    public int[] get_amount_of_variables() {
+        return coefficients;
     }
 }
-
-
--- src/main/java/GUI/MainFrame.java
+++ src/main/java/GUI/MainFrame.java
@@ -1,13 +1,13 @@
 package GUI;
 
 import Engine.Program;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.datatransfer.StringSelection;
+import java.io.*;
 
 public class MainFrame {
     private JPanel main_panel;
@@ -21,89 +21,145 @@ public class MainFrame {
     private JList result_list;
     private JPanel settings_panel;
     private JButton copy_button;
-    private JButton change_equation;
+    private JButton switch_template;
+    private JButton create_mew_template;
     private DefaultListModel<String> result_list_input;
+    private Program program;
 
-    public MainFrame(){
-        Program program = new Program();
+    public MainFrame() {
+        program = new Program();
 
+        // Action to perform when the "Calculate" button is clicked
         calculate_button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String amountBuff = input_field.getText();
-               amountBuff = amountBuff.replaceAll("\\s","");
+                int amount;
+                String amountBuff = input_field.getText().replaceAll("\\s", ""); // Remove spaces
 
-                if(amountBuff.isEmpty()){
-                    JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Enter a value","Error", JOptionPane.ERROR_MESSAGE);
+                if (amountBuff.isEmpty()) {
+                    JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Enter a value", "Error", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
 
-                if(isNumeric(amountBuff) == false){
-                    JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Enter a number, not text","Error", JOptionPane.ERROR_MESSAGE);
+                if (!isNumeric(amountBuff)) {
+                    JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Enter a number, not text", "Error", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
 
-                int amount = Integer.parseInt(amountBuff);
-                result_list.setModel(program.solve_diophantine(amount));
+                try {
+                    amount = Integer.parseInt(amountBuff);
+                } catch (Exception exception) {
+                    JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "The entered value is not compatible with the calculator");
+                    return;
+                }
+
+                result_list.setModel(program.diophantine_bruteforce_setup(amount));
             }
         });
 
+        // Action to copy the result list to the clipboard
         copy_button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
-                String content_to_copy = "";
-                int[] arr = program.get_amount_of_welds();
+                StringBuilder content_to_copy = new StringBuilder();
+                int[] arr = program.get_amount_of_variables();
 
-                for (int i = 0; i < arr.length; i++){
-                    content_to_copy += arr[i] + "\n";
+                for (int i = 0; i < arr.length; i++) {
+                    content_to_copy.append(arr[i]).append("\n");
                 }
 
-                StringSelection strse1 = new StringSelection(content_to_copy);
+                StringSelection strse1 = new StringSelection(content_to_copy.toString());
                 clip.setContents(strse1, strse1);
             }
         });
-        change_equation.addActionListener(new ActionListener() {
+
+        // Action to create a new template
+        create_mew_template.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String buff = JOptionPane.showInputDialog("How many welds would you like to include in the equation?");
-                buff = buff.replaceAll("\\s","");
-                if(!isNumeric(buff) || buff.isEmpty()){
+                File file = create_file();
+                String buff = JOptionPane.showInputDialog("How many variables would you like to include in the equation?");
+                buff = buff.replaceAll("\\s", "");
+
+                if (!isNumeric(buff) || buff.isEmpty()) {
                     JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Error! Please enter numbers only");
                     return;
                 }
+
                 int n = Integer.parseInt(buff);
                 int[] new_values = new int[n];
+
+                // Prompt the user to input each variable
                 for (int i = 0; i < n; i++) {
                     do {
-                        buff = JOptionPane.showInputDialog("Please enter the " + (i + 1) + ":e " + "weld price");
+                        buff = JOptionPane.showInputDialog("Please enter the " + (i + 1) + ":e variable");
                     } while (buff.isEmpty() || !isNumeric(buff));
                     new_values[i] = Integer.parseInt(buff);
                 }
+
+                try {
+                    BufferedWriter bf = new BufferedWriter(new FileWriter(file));
+
+                    // Write the new values to the file
+                    for (int writing_buffert : new_values) {
+                        bf.write(Integer.toString(writing_buffert) + "\n");
+                    }
+
+                    bf.close();
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
                 program.change_equation(new_values);
+            }
+        });
+
+        // Action to switch to a new template (reload Program)
+        switch_template.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                program = new Program();
+            }
+        });
+
+        // Trigger calculation on pressing Enter in the input field
+        input_field.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculate_button.doClick();
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Salary to welds converter");
+        JFrame frame = new JFrame("Diophantine equation solver");
         frame.setContentPane(new MainFrame().main_panel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(800, 600));
         frame.pack();
         frame.setVisible(true);
-
     }
+
 
     public static boolean isNumeric(String strNum) {
         if (strNum == null) {
             return false;
         }
         try {
-            double d = Double.parseDouble(strNum);
+            Double.parseDouble(strNum);
         } catch (NumberFormatException nfe) {
             return false;
         }
         return true;
+    }
+
+    // Create a new file using FileDialog
+    public File create_file() {
+        FileDialog dialog = new FileDialog((Frame) null, "Create a new .txt file");
+        dialog.setMode(FileDialog.SAVE);
+        dialog.setVisible(true);
+        return new File(dialog.getDirectory(), dialog.getFile());
     }
 }
